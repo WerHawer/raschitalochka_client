db5abbbdf3a8c4a3b609b4133679a2cd7231e227
WerHawer <werhawer@gmail.com>
2020-08-30 16:26:29 +0300

    finish nput

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,18 +1,41 @@
 import React from "react";
+import css from "./Input.module.css";
 
 const Input = ({
   value,
-  onChange,
+  onChange = () => null,
   name,
-  onBlur,
-  placeholder,
+  onBlur = () => null,
+  placeholder = "Placeholder",
   type = "text",
   error,
   touched,
   customClass,
   serverError,
 }) => {
-  return <input type="type"></input>;
+  const inputNameBgr = `input__${name}`;
+  const errorStyle = error || serverError ? css.error : undefined;
+
+  return (
+    <div className={css.inputWrapper}>
+      <input
+        className={`${customClass} ${css.input} ${css[inputNameBgr]} ${errorStyle}`}
+        name={name}
+        type={type}
+        value={value}
+        onBlur={onBlur}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+      {touched && error && (
+        <span className={`${css.error} ${css.errorText}`}>{error}</span>
+      )}
+
+      {serverError && (
+        <span className={`${css.error} ${css.errorText}`}>{serverError}</span>
+      )}
+    </div>
+  );
 };
 
 export default Input;
