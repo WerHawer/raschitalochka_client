c9056582fa8b83f5507284dc60460f08ca45b9b3
smdeile <smdeile@gmail.com>
2020-09-06 18:50:23 +0300

    fin styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
-import { Route, Switch, Redirect, Link } from "react-router-dom";
-import TestPage from "pages/TestPage/TestPageContainer";
-import TestRouting from "pages/TestRouting/TestRouting";
+import React from 'react';
+import { Route, Switch, Redirect, Link } from 'react-router-dom';
+import TestPage from 'pages/TestPage/TestPageContainer';
+import TestRouting from 'pages/TestRouting/TestRouting';
+import LoginPage from 'pages/LoginPage/LoginPage';
 
 const App = () => {
+  const isAuth = false;
   return (
     <>
-      <nav>
-        <Link to="/" style={{ marginRight: 20 }}>
-          Home
-        </Link>
-        <Link to="/test">Test Page</Link>
-      </nav>
+      {isAuth && (
+        <nav>
+          <Link to="/" style={{ marginRight: 20 }}>
+            Home
+          </Link>
+          <Link to="/test">Test Page</Link>
+        </nav>
+      )}
 
       <Switch>
         <Route path="/" exact component={TestPage} />
         <Route path="/test" component={TestRouting} />
+        <Route path="/signin" component={LoginPage} />
         <Redirect to="/" />
       </Switch>
     </>
