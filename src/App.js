611777ec8e8c1cc7f48210e116d17257d7716435
WerHawer <werhawer@gmail.com>
2020-09-08 10:25:45 +0300

    delete test pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import React from "react";
-import { Route, Switch, Redirect, Link } from "react-router-dom";
-import TestPage from "pages/TestPage/TestPageContainer";
-import TestRouting from "pages/TestRouting/TestRouting";
+import React from 'react';
 
 const App = () => {
   return (
     <>
-      <nav>
-        <Link to="/" style={{ marginRight: 20 }}>
-          Home
-        </Link>
-        <Link to="/test">Test Page</Link>
-      </nav>
-
-      <Switch>
-        <Route path="/" exact component={TestPage} />
-        <Route path="/test" component={TestRouting} />
-        <Redirect to="/" />
-      </Switch>
+      <h1>App</h1>
     </>
   );
 };
