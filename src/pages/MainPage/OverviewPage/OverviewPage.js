15594ad32961174e142db2441500e8ea8dc4f3af
andryxins <andryxins@icloud.com>
2020-09-08 11:25:28 +0300

    added mobile and tablet view

--- src/pages/MainPage/OverviewPage/OverviewPage.js
+++ src/pages/MainPage/OverviewPage/OverviewPage.js
@@ -4,21 +4,22 @@ import NavBar from '../../../components/NavBar/NavBar';
 
 const OverviewPage = () => (
   <div className={Styles.pageWrapper}>
-    <NavBar />
-    <div className={Styles.container}>
-      <div className={Styles.balanceBar}>
-        {/* Place for balance component */}
-        {/* ////// */}
-      </div>
-      <ul className={Styles.transactionsList}>
-        <li className={Styles.transactionsListItem}></li>
-        <li className={Styles.transactionsListItem}>
-          {/* Place for transactions, 
-          use this <li> for wrapping each tranaction  */}
-        </li>
-      </ul>
+    <div>
+      <NavBar balance={24000} />
     </div>
-    <button className={Styles.AddTransaction}>+</button>
+    <div className={Styles.balanceBar}>
+      {/* Place for balance component */}
+      {/* ////// */}
+    </div>
+    <ul className={Styles.transactionsList}>
+      <li className={Styles.transactionsListItem}>
+        {/* Place for transactions, 
+          use this <li> for wrapping each transaction 
+          or replace <ul> with your component and add <ul>'s classname */}
+      </li>
+    </ul>
+    <div className={Styles.currencyComponent}></div>
+    <button className={Styles.addTransaction}>+</button>
     <footer className={Styles.mobileFooter}></footer>
   </div>
 );
