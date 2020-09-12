b71e0801913ae1b3fb6b2a8774d161d05e27f4a6
MaxAlter <alter0075926@gmail.com>
2020-09-12 15:40:32 +0300

    add to component Select

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Select from './components/Select/Select';
+
 const App = () => {
   const isAuth = false;
 
   return (
     <>
+      <Select />
       <h1>App</h1>
     </>
   );
